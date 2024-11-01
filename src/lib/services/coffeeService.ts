ca1e41785178ca980dceb585ef7cafd797549ab5
hliebovkyrylo <kirilglebov06@gmail.com>
2024-11-01 18:44:30 +0200

    perf: optimized the code of the coffee purchase function

--- src/lib/services/coffeeService.ts
+++ src/lib/services/coffeeService.ts
@@ -26,49 +26,62 @@ export class CoffeeService {
   }
 
   async processOrder(orderItems: OrderItem[]) {
-    let totalAmount = 0;
-    const purchasedCoffees: PurchasedCoffee[] = [];
-
     const getCoffeeAndValidateStock = async (
       tx: Prisma.TransactionClient,
       item: OrderItem
     ) => {
       const coffee = await tx.coffee.findUnique({
         where: {
-          id: item.id
-        }
+          id: item.id,
+        },
       });
-      if (!coffee) throw new NotFoundError(`Coffee with ID '${item.id}' not found`);
+
+      if (!coffee)
+        throw new NotFoundError(`Coffee with ID '${item.id}' not found`);
+
       if (coffee.quantity < item.quantity)
         throw new InsufficientError(
           `Insufficient stock for ${coffee.name}. Available: ${coffee.quantity} packages`
         );
-      return coffee;
+
+      return { coffee, item };
     };
 
     try {
-      await prisma.$transaction(async (tx: Prisma.TransactionClient) => {
-        for (const item of orderItems) {
-          const coffee = await getCoffeeAndValidateStock(tx, item);
+      const result = await prisma.$transaction(
+        async (tx: Prisma.TransactionClient) => {
+          const processedItems = await Promise.all(
+            orderItems.map((item) => getCoffeeAndValidateStock(tx, item))
+          );
 
-          totalAmount += coffee.purchasePrice * item.quantity;
+          const totalAmount = processedItems.reduce(
+            (acc, { coffee, item }) =>
+              acc + coffee.purchasePrice * item.quantity,
+            0
+          );
 
-          await tx.coffee.update({
-            where: { id: coffee.id },
-            data: { quantity: { decrement: item.quantity } },
-          });
+          await Promise.all(
+            processedItems.map(({ coffee, item }) =>
+              tx.coffee.update({
+                where: { id: coffee.id },
+                data: { quantity: { decrement: item.quantity } },
+              })
+            )
+          );
 
-          purchasedCoffees.push({
+          const purchasedCoffees = processedItems.map(({ coffee, item }) => ({
             id: coffee.id,
             name: coffee.name,
             imageUrl: coffee.imageUrl,
             quantity: item.quantity,
             price: coffee.purchasePrice,
-          });
-        }
-      });
+          }));
 
-      return { totalAmount, purchasedItems: purchasedCoffees };
+          return { totalAmount, purchasedItems: purchasedCoffees };
+        }
+      );
+
+      return result;
     } catch (error) {
       throw error;
     }
