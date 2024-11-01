a5995d571d1dde6bb37accfd1a6768e32b8c295a
hliebovkyrylo <kirilglebov06@gmail.com>
2024-11-01 10:45:08 +0200

    feat: made types for order

---
+++ src/@types/order.ts
@@ -0,0 +1,12 @@
+export interface OrderItem {
+  name: string;
+  quantity: number;
+}
+
+export interface PurchasedCoffee {
+  id: string;
+  name: string;
+  imageUrl: string;
+  quantity: number;
+  price: number;
+}
